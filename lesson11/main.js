8d897bd84e33b2be634f42ecb11990fe0b8266e3
tamas <tamas@localhost.localdomain>
2020-02-26 21:50:44 +0100

    lesson11

---
+++ lesson11/main.js
@@ -0,0 +1,21 @@
+const express = require("express"),
+    hc = require("./controllers/homeControllers"),
+    error = require("./controllers/errorController"),
+    layouts = require("express-ejs-layouts"),
+    app = express()
+
+app.set("port", process.env.PORT || 3000) // application settings property
+
+// initiate using templating
+app.set("view engine", "ejs") // to set the view engine as 'ejs'
+
+app.use(layouts) //to use layouts as an additional layer of middleware
+app.get("/name/:myName", hc.respondWithName)
+app.get("/contact", hc.contactInformation)
+
+// app.use(error.errorHandle)
+app.use(error.respondNoResourceFound)
+app.use(error.respondInternalError)
+app.listen(app.get("port"), () => {
+    console.log(`Server listening on: ${ app.get("port") }`)
+})
