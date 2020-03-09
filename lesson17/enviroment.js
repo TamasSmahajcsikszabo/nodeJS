4dc8f66e50f250ea842454281cefc1f9520334f0
tamas <tamas@localhost.localdomain>
2020-03-09 14:37:50 +0100

    update

---
+++ lesson17/enviroment.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose"),
+  Subscriber = require("./models/subscriber");
+mongoose.connect(
+  "mongodb://localhost:27017/recipe_db",
+  {useNewUrlParser: true}
+);
+mongoose.Promise = global.Promise;
