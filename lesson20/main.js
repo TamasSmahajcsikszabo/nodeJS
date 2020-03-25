21c40396001d807e36cdeb705f0e23f836bf36ae
tamas <tamas@localhost.localdomain>
2020-03-25 12:09:07 +0100

    update and delete functionalities

--- lesson20/main.js
+++ lesson20/main.js
@@ -31,7 +31,6 @@ router.use(methodOverride("_method", {
     methods: ["POST", "GET"]
 }));
 
-
 // home
 router.get("/", homeController.index)
 
@@ -47,12 +46,14 @@ router.get("/contact", subscriberController.new)
 router.post("/subscribe", subscriberController.create, subscriberController.redirectView)
 router.get("/subscribers/:id", subscriberController.show, subscriberController.showView)
 
-
 //users
 router.get("/users", userController.index, userController.indexView)
 router.get("/users/new", userController.new)
 router.post("/users/create", userController.create, userController.redirectView)
 router.get("/users/:id", userController.show, userController.showView)
+router.get("/users/:id/edit", userController.edit)
+router.put("/users/:id/update", userController.update, userController.redirectView)
+router.delete("/users/:id/delete", userController.delete, userController.redirectView)
 
 router.use(errorControllers.pageNotFoundError)
 router.use(errorControllers.internalServerError)
