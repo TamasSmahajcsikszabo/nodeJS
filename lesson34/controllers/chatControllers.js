1aba852efca75f645d37eb76779ca0b2dd8a021a
tamas <tamas@localhost.localdomain>
2020-05-18 21:22:03 +0200

    heroku update

---
+++ lesson34/controllers/chatControllers.js
@@ -0,0 +1,47 @@
+// server side, what the client is listening for
+// data from here is emitted to all clients
+const Message = require("../models/message.js"),
+    User = require("../models/user.js");
+
+module.exports = io => {
+    io.on("connection", client  => {
+        
+        client.on("user connected", data => {
+            client.broadcast.emit("user connected", data.user);
+        })
+
+        Message.find({})
+            .sort({ createdAt: -1 })
+            .limit(10)
+            .then(messages  => {
+                client.emit("load all messages", messages.reverse());
+            })
+        
+        client.on("disconnect", data  => {
+            client.broadcast.emit("user disconnected");
+        });
+
+        client.on("message", data  => {
+            // let date = Date().toString();
+            // let formattedDate = date.substring(0, date.indexOf("GMT"))
+            let messageAttributes = {
+                content: data.content,
+                user: data.userId,
+                userName: data.userName
+            },
+                m = new Message(messageAttributes);
+
+            User.find({ "_id" : messageAttributes.user})
+                .then(user  => {
+                    console.log(`${messageAttributes.userName} found in database`);
+                        m.save()
+                            .then(()  => {
+                                console.log(`message saved to database`)
+                                io.emit("message", messageAttributes);
+                            })
+                            .catch(error  => console.log(`error: ${error.message}`));
+                })
+                .catch(error  => console.log(`error when saving: ${error.message}`));
+        });
+    });
+};
---
+++ lesson34/controllers/subscribersController.js
@@ -0,0 +1,136 @@
+const Subscriber = require("../models/subscriber.js"),
+    httpStatus = require("http-status-codes"),
+getSubscriberParams = body  => {
+    return {
+            username: body.username,
+            name: body.name,
+            email: body.email,
+            zipCode: body.zipCode
+        
+    }
+};
+
+module.exports = {
+    index: (req, res) => {
+    Subscriber.find({})
+        .exec()
+        .then((subscribers) => {
+            res.locals.subscribers = subscribers;
+            if (req.originalUrl.indexOf("api") != -1) {
+                res.json(res.locals.subscribers);
+            } else {
+                res.render("subscribers/index", {
+                    subscribers: subscribers
+                });
+            }
+        })
+        .catch((error) => {
+            console.log(error.message)
+            return []
+        })
+        // .then(() => {
+        //     console.log("promise complete")
+        // })
+},
+
+    new: (req, res) => {
+        res.render("subscribers/new");
+    },
+
+    create: (req, res, next)  => {
+        let subscriberParams = getSubscriberParams(req.body);
+
+    Subscriber.create(subscriberParams)
+            .then(subscriber =>  {
+                req.flash('success', `Subscribed successfully!`)
+                res.locals.redirect = "/subscribers";
+                res.locals.subscriber = subscriber;
+                next();
+            })
+            .catch(error  => {
+                console.log(`Error saving subscriber: ${error.message}`);
+                res.locals.redirect = "/subscribers/new";
+                req.flash('error', `Error occured: ${error.message}`);
+                next();
+            });
+    },
+    redirectView: (req, res, next) => {
+        let redirectPath = res.locals.redirect;
+        if (redirectPath) res.redirect(redirectPath);
+        else next();
+    },
+
+    show: (req, res, next) => {
+        let subscriberId = req.params.id;
+        Subscriber.findById(subscriberId)
+            .then( subscriber => {
+                res.locals.subscriber = subscriber;
+                next();
+            })
+            .catch(error => {
+                console.log(`Error fetching subscriber by ID: ${error.message}`);
+            })
+    },
+
+    showView: (req, res) => {
+        if (req.query.format == "json") {
+            res.json(res.locals.subscriber);
+        } else{
+        res.render("subscribers/show");
+    }
+    },
+
+    edit: (req, res, next) => {
+        let subscriberId = req.params.id;
+        Subscriber.findById(subscriberId)
+            .then(subscriber => {
+                res.render("subscribers/edit", {
+                    subscriber: subscriber
+                })
+            })
+            .catch(error  => {
+                console.log(`Error fetching subscriber by ID: ${error.message}`);
+            });
+    },
+
+    update: (req, res, next) => {
+        let subscriberId = req.params.id;
+        subscriberParams = getSubscriberParams(req.body);
+
+       Subscriber.findByIdAndUpdate(subscriberId, {
+            $set: subscriberParams
+        })
+            .then(subscriber => {
+                res.locals.redirect = `/subscribers/${subscriberId}`;
+                res.locals.subscriber = subscriber;
+                req.flash('success', `Successfully updated data for subscriber ${subscriber.name}`)
+                next();
+            })
+            .catch(error  => {
+                console.log(`Error updating subscriber by ID: ${error.message}`);
+                req.flash('error', `While updating error encountered: ${error.message}`)
+                next();
+            });
+    },
+    delete: (req, res, next)  => {
+        let subscriberId = req.params.id;
+        Subscriber.findByIdAndRemove(subscriberId)
+            .then(() => {
+                res.locals.redirect = "/subscribers";
+                req.flash('success', `Subscriber deleted!`)
+                next();
+            })
+            .catch(error  => {
+                console.log('Error deleting subscribers by ID: ${error.message}');
+                res.locals.redirect = "/subscribers";
+                req.flash('error', `Error while deletingsubscriber!`)
+                next();
+            });
+    },
+    respondJSON: (req, res) => {
+        res.json({
+            status: httpStatus.OK,
+            data: res.locals // the response's locals object already has attached data by this point
+        })
+    }
+}
